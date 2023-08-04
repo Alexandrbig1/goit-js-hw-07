b9802a0bef919edb5820ffc934e9c8bea8363cd3
Alex Smagin <alexsmagin1@gmail.com>
2023-08-04 13:41:45 -0500

    update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,13 +3,10 @@ import { galleryItems } from "./gallery-items.js";
 
 const imagesMenuEl = document.querySelector(".gallery");
 
-// console.log(images);
-
-function createImagesEl(img) {
-  img.map(({ preview, original, description }) =>
-    imagesMenuEl.insertAdjacentHTML(
-      "beforeend",
-      `<li class="gallery__item">
+function createImages(image) {
+  return image
+    .map(
+      ({ preview, original, description }) => `<li class="gallery__item">
       <a class="gallery__link" target="_blank" href="${original}">
         <img
           class="gallery__image"
@@ -20,40 +17,26 @@ function createImagesEl(img) {
       </a>
     </li>`
     )
-  );
+    .join("");
 }
 
-createImagesEl(galleryItems);
+imagesMenuEl.insertAdjacentHTML("beforeend", createImages(galleryItems));
 
-// const instance = basicLightbox.create(`
-//     <img src="assets/images/image.png" width="800" height="600">
-// `);
+imagesMenuEl.addEventListener("click", openModalPhotoOnClick);
 
-// instance.show();
+function openModalPhotoOnClick(e) {
+  e.preventDefault();
 
-// Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
-// Реалізація делегування на ul.gallery і отримання url великого зображення.
-// Підключення скрипту і стилів бібліотеки модального вікна basicLightbox. Використовуй CDN сервіс jsdelivr і додай у проект посилання на мініфіковані (.min) файли бібліотеки.
-// Відкриття модального вікна по кліку на елементі галереї. Для цього ознайомся з документацією і прикладами.
-// Заміна значення атрибута src елемента <img> в модальному вікні перед відкриттям. Використовуй готову розмітку модального вікна із зображенням з прикладів бібліотеки basicLightbox.
+  const currentImage = e.target.dataset.source;
+  const instance = basicLightbox.create(`
+    <img src="${currentImage}" width="800" height="600">
+`);
 
-// 111
+  if (e.target !== e.currentTarget) instance.show();
 
-// Посилання на оригінальне зображення повинно зберігатися в data-атрибуті source на елементі <img>, і вказуватися в href посилання. Не додавай інші HTML теги або CSS класи, крім тих, що містяться в цьому шаблоні.
+  window.addEventListener("keydown", closeOnEscClick);
 
-// 222
-
-/* <li class="gallery__item">
-  <a class="gallery__link" href="large-image.jpg">
-    <img
-      class="gallery__image"
-      src="small-image.jpg"
-      data-source="large-image.jpg"
-      alt="Image description"
-    />
-  </a>
-</li>; */
-
-// 333
-
-// Зверни увагу на те, що зображення обгорнуте посиланням, отже по кліку за замовчуванням користувач буде перенаправлений на іншу сторінку. Заборони цю поведінку за замовчуванням.
+  function closeOnEscClick(e) {
+    if (e.code === "Escape") instance.close();
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const imagesMenuEl = document.querySelector(".gallery");
+
+function createImages(image) {
+  return image
+    .map(
+      ({ preview, original, description }) => `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>
+</li>`
+    )
+    .join("");
+}
+
+imagesMenuEl.insertAdjacentHTML("beforeend", createImages(galleryItems));
+
+imagesMenuEl.addEventListener("click", openModalPhotoOnClick);
+
+function openModalPhotoOnClick(e) {
+  e.preventDefault();
+
+  const lightbox = new SimpleLightbox(".gallery__link", {
+    captionDelay: 250,
+    captionsData: "alt",
+  });
+}
